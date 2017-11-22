2a6b79a2c5315c1ef3975cad11a49ea78ad3bcdb
Leo D'Arcy <leorools@hotmail.com>
2017-11-22 13:00:02 +0000

    Update Module to Meet current PSSA guidelines (#29)
    There are a number of issues with the current module when run through the standard battery of tests in Appveyor. This PR goes through and fixes these. They are all very minor, mainly formatting and Parameter changes and there shouldn't be any functional impact to the module.
    
    I have also updated the copyright information as per the guidelines and fixed the failing unit tests while bringing them in line with the latest testing template.

--- tests/unit/MSFT_xRDRemoteApp.tests.ps1
+++ tests/unit/MSFT_xRDRemoteApp.tests.ps1
@@ -1,81 +1,96 @@
-$Global:DSCModuleName      = '.\xRemoteDesktopSessionHost' # Example xNetworking
-$Global:DSCResourceName    = 'MSFT_xRDRemoteApp' # Example MSFT_xFirewall
+$script:DSCModuleName      = '.\xRemoteDesktopSessionHost'
+$script:DSCResourceName    = 'MSFT_xRDRemoteApp'
 
 #region HEADER
-[String] $moduleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path))
-if ( (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
-     (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
+
+# Unit Test Template Version: 1.2.1
+$script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
+Write-Output @('clone','https://github.com/PowerShell/DscResource.Tests.git',"'"+(Join-Path -Path $script:moduleRoot -ChildPath '\DSCResource.Tests')+"'")
+
+if ( (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
+     (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
 {
-    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'))
+    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests'),'--verbose')
 }
-else
-{
-    & git @('-C',(Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'),'pull')
-}
-Import-Module (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1') -Force
+
+Import-Module -Name (Join-Path -Path $script:moduleRoot -ChildPath (Join-Path -Path 'DSCResource.Tests' -ChildPath 'TestHelper.psm1')) -Force
+
 $TestEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $Global:DSCModuleName `
-    -DSCResourceName $Global:DSCResourceName `
-    -TestType Unit 
-#endregion
+    -DSCModuleName $script:DSCModuleName `
+    -DSCResourceName $script:DSCResourceName `
+    -TestType Unit
+
+#endregion HEADER
+
+function Invoke-TestSetup {
+
+}
+
+function Invoke-TestCleanup {
+    Restore-TestEnvironment -TestEnvironment $TestEnvironment
+
+}
 
 # Begin Testing
 
 try
 {
-    InModuleScope $Global:DSCResourceName {
 
-        #region Pester Test Initialization
-        # TODO: Optopnal Load Mock for use in Pester tests here...
-        #endregion
+    Invoke-TestSetup
 
-        $testInvalidCollectionName = 'InvalidCollectionName longer than 15'
+    InModuleScope $script:DSCResourceName {
+        $script:DSCResourceName    = 'MSFT_xRDRemoteApp'
+
+        $testInvalidCollectionName = 'InvalidCollectionNameLongerThan15'
                 
         Import-Module RemoteDesktop -Force
         
         #region Function Get-TargetResource
-        Describe "$($Global:DSCResourceName)\Get-TargetResource" {
+        Describe "$($script:DSCResourceName)\Get-TargetResource" {
             Context "Parameter Values,Validations and Errors" {
 
                 It "Should error when CollectionName length is greater than 15" {
-                    {Get-TargetResource `
-                        -CollectionName = $testInvalidCollectionName `
-                        -DisplayName = 'name' `
-                        -FilePath = 'path' `
-                        -Alias = 'alias' `
-                     } | should throw
+                    {
+                        Get-TargetResource `
+                        -CollectionName $testInvalidCollectionName `
+                        -DisplayName 'name' `
+                        -FilePath 'path' `
+                        -Alias 'alias' `
+                     } | Should Throw
                 }
             }
         }
         #endregion
 
         #region Function Set-TargetResource
-        Describe "$($Global:DSCResourceName)\Set-TargetResource" {
+        Describe "$($script:DSCResourceName)\Set-TargetResource" {
             Context "Parameter Values,Validations and Errors" {
 
                 It "Should error when CollectionName length is greater than 15" {
-                    {Set-TargetResource `
-                        -CollectionName = $testInvalidCollectionName `
-                        -DisplayName = 'name' `
-                        -FilePath = 'path' `
-                        -Alias = 'alias' `
-                     } | should throw
+                    {
+                        Set-TargetResource `
+                        -CollectionName $testInvalidCollectionName `
+                        -DisplayName 'name' `
+                        -FilePath 'path' `
+                        -Alias 'alias' `
+                     } | Should Throw
                 }
             }
         }
         #endregion
 
         #region Function Test-TargetResource
-        Describe "$($Global:DSCResourceName)\Test-TargetResource" {
+        Describe "$($script:DSCResourceName)\Test-TargetResource" {
             Context "Parameter Values,Validations and Errors" {
 
                 It "Should error when CollectionName length is greater than 15" {
-                    {Test-TargetResource `
-                        -CollectionName = $testInvalidCollectionName `
-                        -DisplayName = 'name' `
-                        -FilePath = 'path' `
-                        -Alias = 'alias' `
-                    } | should throw
+                    {
+                        Test-TargetResource `
+                        -CollectionName $testInvalidCollectionName `
+                        -DisplayName 'name' `
+                        -FilePath 'path' `
+                        -Alias 'alias' `
+                    } | Should Throw
                 }            
             }
         }
@@ -85,6 +100,6 @@ try
 finally
 {
     #region FOOTER
-    Restore-TestEnvironment -TestEnvironment $TestEnvironment
+    Invoke-TestCleanup
     #endregion
 }
--- tests/unit/MSFT_xRDSessionCollectionConfiguration.tests.ps1
+++ tests/unit/MSFT_xRDSessionCollectionConfiguration.tests.ps1
@@ -1,40 +1,50 @@
-$Global:DSCModuleName      = '.\xRemoteDesktopSessionHost' # Example xNetworking
-$Global:DSCResourceName    = 'MSFT_xRDSessionCollectionConfiguration' # Example MSFT_xFirewall
+$script:DSCModuleName      = '.\xRemoteDesktopSessionHost'
+$script:DSCResourceName    = 'MSFT_xRDSessionCollectionConfiguration'
 
 #region HEADER
-[String] $moduleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path))
-if ( (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
-     (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
+
+# Unit Test Template Version: 1.2.1
+$script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
+Write-Output @('clone','https://github.com/PowerShell/DscResource.Tests.git',"'"+(Join-Path -Path $script:moduleRoot -ChildPath '\DSCResource.Tests')+"'")
+
+if ( (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
+     (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
 {
-    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'))
+    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests'),'--verbose')
 }
-else
-{
-    & git @('-C',(Join-Path -Path $moduleRoot -ChildPath '\DSCResource.Tests\'),'pull')
-}
-Import-Module (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1') -Force
+
+Import-Module -Name (Join-Path -Path $script:moduleRoot -ChildPath (Join-Path -Path 'DSCResource.Tests' -ChildPath 'TestHelper.psm1')) -Force
+
 $TestEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $Global:DSCModuleName `
-    -DSCResourceName $Global:DSCResourceName `
-    -TestType Unit 
-#endregion
+    -DSCModuleName $script:DSCModuleName `
+    -DSCResourceName $script:DSCResourceName `
+    -TestType Unit
+
+#endregion HEADER
+
+function Invoke-TestSetup {
+
+}
+
+function Invoke-TestCleanup {
+    Restore-TestEnvironment -TestEnvironment $TestEnvironment
+
+}
 
 # Begin Testing
 
 try
 {
-    InModuleScope $Global:DSCResourceName {
+    Invoke-TestSetup
+    InModuleScope $script:DSCResourceName {
+        $script:DSCResourceName    = 'MSFT_xRDSessionCollectionConfiguration'
 
-        #region Pester Test Initialization
-        # TODO: Optopnal Load Mock for use in Pester tests here...
-        #endregion
-
-        $testInvalidCollectionName = 'InvalidCollectionName longer than 15'
+        $testInvalidCollectionName = 'InvalidCollectionNameLongerThan15'
         
         Import-Module RemoteDesktop -Force
         
         #region Function Get-TargetResource
-        Describe "$($Global:DSCResourceName)\Get-TargetResource" {
+        Describe "$($script:DSCResourceName)\Get-TargetResource" {
             Context "Parameter Values,Validations and Errors" {
 
                 It "Should error when CollectionName length is greater than 15" {
@@ -46,7 +56,7 @@ try
         #endregion
 
         #region Function Set-TargetResource
-        Describe "$($Global:DSCResourceName)\Set-TargetResource" {
+        Describe "$($script:DSCResourceName)\Set-TargetResource" {
             Context "Parameter Values,Validations and Errors" {
 
                 It "Should error when CollectionName length is greater than 15" {
@@ -58,7 +68,7 @@ try
         #endregion
 
         #region Function Test-TargetResource
-        Describe "$($Global:DSCResourceName)\Test-TargetResource" {
+        Describe "$($script:DSCResourceName)\Test-TargetResource" {
             Context "Parameter Values,Validations and Errors" {
 
                 It "Should error when CollectionName length is greater than 15" {
@@ -73,6 +83,6 @@ try
 finally
 {
     #region FOOTER
-    Restore-TestEnvironment -TestEnvironment $TestEnvironment
+    Invoke-TestCleanup
     #endregion
 }
