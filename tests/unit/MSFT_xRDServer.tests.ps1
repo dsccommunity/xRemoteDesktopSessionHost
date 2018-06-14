ac1011c8bc15dcf730aa519876c6e7747407bb69
Daniël Both <danielboth@live.nl>
2018-06-14 21:49:17 +0000

    Adding tests for MSFT_xRDServer Get-TargetResource and Test-TargetResource

--- tests/unit/MSFT_xRDServer.tests.ps1
+++ tests/unit/MSFT_xRDServer.tests.ps1
@@ -42,6 +42,93 @@ try
         
         Import-Module RemoteDesktop -Force
 
+        Describe "$($script:DSCResourceName)\Get-TargetResource" {
+            Mock -CommandName Get-RDServer -MockWith {
+                [pscustomobject]@{
+                    Server = 'connectionbroker.lan'
+                    Roles = @(
+                        'RDS-CONNECTION-BROKER'
+                    )
+                }
+            }
+            
+            Mock -CommandName Get-RDDeploymentGatewayConfiguration -MockWith {
+                [pscustomobject]@{
+                    GatewayExternalFqdn = 'testgateway.external.fqdn'
+                }
+            }
+            
+            It 'Given a server that does not exist in the deployment, Get returns nothing' {
+                Get-TargetResource -Server does.not.exist -ConnectionBroker connectionbroker.lan -Role RDS-Connection-Broker | Should BeNullOrEmpty
+            }
+            
+            $getResult = Get-TargetResource -Server connectionbroker.lan -ConnectionBroker connectionbroker.lan -Role RDS-Connection-Broker
+            It 'Given a server connectionbroker.lan with the role RDS-CONNECTION-BROKER in the deployment, get returns property <Property> for this server' {
+                param (
+                    [string]$Property
+                )
+                
+                $getResult.$Property | Should Not BeNullOrEmpty
+            } -TestCases @(
+                @{
+                    Property = 'ConnectionBroker'
+                }
+                @{
+                    Property = 'Server'
+                }
+                @{
+                    Property = 'Role'
+                }
+            )
+            
+            Mock -CommandName Get-RDServer -MockWith {
+                [pscustomobject]@{
+                    Server = 'connectionbroker.lan'
+                    Roles = @(
+                        'RDS-CONNECTION-BROKER'
+                        'RDS-GATEWAY'
+                    )
+                }
+            }
+            
+            $getResult = Get-TargetResource -ConnectionBroker connectionbroker.lan -Server connectionbroker.lan -Role RDS-Gateway
+            It 'Given a server with gateway role, the External Gateway FQDN is returned by Get' {
+                $getResult.GatewayExternalFqdn | Should Be 'testgateway.external.fqdn'
+            }
+        }
+        
+        Describe "$($script:DSCResourceName)\Test-TargetResource" {
+            Mock -CommandName Get-RDServer -MockWith {
+            
+                if($Role -eq 'RDS-Connection-Broker') {
+                    [pscustomobject]@{
+                        Server = 'connectionbroker.lan'
+                        Roles = @(
+                            'RDS-CONNECTION-BROKER'
+                        )
+                    }
+                }
+            }
+            
+            Mock -CommandName Get-RDDeploymentGatewayConfiguration -MockWith {
+                [pscustomobject]@{
+                    GatewayExternalFqdn = 'testgateway.external.fqdn'
+                }
+            }
+            
+            It 'Given a new server in the deployment, test returns false' {
+                Test-TargetResource -Server does.not.exist -ConnectionBroker connectionbroker.lan -Role RDS-Connection-Broker | Should be $false
+            }
+            
+            It 'Given an existing server in the deployment, but with an unassigned role, test returns false' {
+                Test-TargetResource -Server connectionbroker.lan -ConnectionBroker connectionbroker.lan -Role RDS-Gateway | Should be $false
+            }
+            
+            It 'Given an existing server in the deployment, with an existing role, test returns true' {
+                Test-TargetResource -Server connectionbroker.lan -ConnectionBroker connectionbroker.lan -Role RDS-Connection-Broker | Should be $true
+            }
+        }
+
         #region Function Set-TargetResource
         Describe "$($script:DSCResourceName)\Set-TargetResource" {
             Context "Parameter Values,Validations and Errors" {
